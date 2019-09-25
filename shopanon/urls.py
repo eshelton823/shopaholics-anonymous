35472c3c7d5c055174fcda2f30ae332451610696
Lukas Curtis <llc6wh@virginia.edu>
2019-09-25 16:26:02 -0400

    signup form and models

--- shopanon/urls.py
+++ shopanon/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url, include
 from django.urls import path
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^', include('users.urls')),
 ]
