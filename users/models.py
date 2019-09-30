17141e34f23d7c2fab987b3638aefbdc0a162833
Jason Ashley <jasona99@outlook.com>
2019-09-30 15:13:05 -0400

    Doc test

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    #personal
+    #personal -- updated on sign-in and profile
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     email = models.EmailField(max_length = 50)
