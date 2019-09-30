4acce4311584df04edcc137221ef099ce957dbac
Jason Ashley <jasona99@outlook.com>
2019-09-30 10:24:17 -0400

    Start on PostgreSQL migration.

--- shopanon/settings.py
+++ shopanon/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,8 +78,10 @@ WSGI_APPLICATION = 'shopanon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "shopaholics-anonymous",
+        'HOST': "localhost",
+        "PORT": "5432",
     }
 }
 
@@ -123,3 +126,6 @@ STATIC_URL = '/static/'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+# Connect to PostgreSQL
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
