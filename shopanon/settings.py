f3571eea93a872d0bdb7c4937eebe227038cc85c
Jason Ashley <jasona99@outlook.com>
2019-10-13 18:10:29 -0400

    Working Google Auth.

--- shopanon/settings.py
+++ shopanon/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'shopanon.urls'
@@ -91,17 +92,35 @@ DATABASES = {
 psql="postgres://kshnflpzmhwcye:3bc77f34ff3114a5e82ecbfd237c91e6824934cc8b367c763226aabde17ee491@ec2-54-221-238-248.compute-1.amazonaws.com:5432/d1k3n8fum6qq6h"
 DATABASES['default'] = dj_database_url.config(default=psql, conn_max_age=600, ssl_require=True)
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
 AUTHENTICATION_BACKENDS = (
- 'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
- 'social_core.backends.google.GoogleOpenId',  # for Google authentication
  'social_core.backends.google.GoogleOAuth2',  # for Google authentication
  'django.contrib.auth.backends.ModelBackend',
 )
 
-LOGIN_URL = 'login'
+SOCIAL_AUTH_GOOGLE_OAUTH2_IGNORE_DEFAULT_SCOPE = True
+SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = [
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/userinfo.profile'
+]
+
+LOGIN_URL = '/auth/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = "/"
-SOCIAL_AUTH_URL_NAMESPACE = 'social'
+
+SOCIAL_AUTH_STRATEGY = 'social_django.strategy.DjangoStrategy'
+SOCIAL_AUTH_STORAGE = 'social_django.models.DjangoStorage'
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '468447031403-2ufnan3jvf8im4ap5jgs34jnj2fe7p94'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'CHvIa6iwZiKswPgex7rJnyHh'
