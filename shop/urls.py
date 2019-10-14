c95f1c74249c509ec9659ca74b5faa11df2fd33d
Jason Ashley <jasona99@outlook.com>
2019-10-14 13:32:06 -0400

    Removed second template by switching to dynamic names.

--- shop/urls.py
+++ shop/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "shop"
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('dashboard', views.dashboard, name='dashboard'),
