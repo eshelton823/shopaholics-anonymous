4028ea4cedb87da4d2f04d6c0db805cd9ae0f62c
Jason Ashley <jasona99@outlook.com>
2019-10-14 23:30:40 -0400

    Static root update

--- shopanon/settings.py
+++ shopanon/settings.py
@@ -175,12 +175,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
 # Activate Django-Heroku.
 if '/app' in os.environ['HOME']:
