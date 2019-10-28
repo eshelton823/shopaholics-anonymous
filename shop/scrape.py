027f55ead5f382b5697ad4cb9a2312f9b9e03914
Jason Ashley <jasona99@outlook.com>
2019-10-28 14:22:41 -0400

    Added complete search function.

--- shop/scrape.py
+++ shop/scrape.py
@@ -9,8 +9,13 @@ def getItems(query):
     r = requests.get(url, cookies=cookie)
     soup = BeautifulSoup(r.text, 'html.parser')
     items = soup.findAll("div", {"data-tl-id" : re.compile('ProductTileListView-*')})
+    parsed = []
     for item in items:
         title = item.find("a", {'class':'product-title-link line-clamp line-clamp-2'})
-        print(title.decode_contents())
-
-getItems("milk")
+        img = item.find("img", {'data-pnodetype':"item-pimg"})
+        price = item.find('span', {'class':'price-group'})
+        if title is None or img is None or price is None:
+            continue
+        #print(title.decode_contents(), img['src'], price.decode_contents())
+        parsed.append({'title':title.get_text(), 'image':img['src'], 'price':price.get_text()})
+    return parsed
