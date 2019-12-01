f7c017608d50905d18a1ddbee9a9d301af5fd8bd
png <jasona99@outlook.com>
2019-12-01 13:11:22 -0500

    Debugging off, maybe too much logging.

--- shopanon/settings.py
+++ shopanon/settings.py
@@ -30,8 +30,38 @@ SECRET_KEY = 'q=cmugn0-(28i^rt#e(!zmr#0welvba-=tzmw18-(o)1sd2zk#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+DEBUG_PROPAGATE_EXCEPTIONS=True
 
 ALLOWED_HOSTS = ["shopaholics-anonymous.herokuapp.com", "shopanontest.herokuapp.com", "localhost", "127.0.0.1"]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+     'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
 
 
 # Application definition
