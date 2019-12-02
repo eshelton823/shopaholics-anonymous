4c3071fc92db6a34b155e09c9524e58718a9e67c
rjpark1124 <richard.park11@gmail.com>
2019-12-01 19:05:19 -0500

    UI Changes... Chat Enter button

--- chat/static/chat/scripts/rooms.js
+++ chat/static/chat/scripts/rooms.js
@@ -150,4 +150,12 @@ $form.on("submit", function(e) {
     $input.val("");
   }
 });
+$form.on(‘keypress’,function(e) {
+  if (e.which == 13) {
+    if (roomChannel && $input.val().trim().length > 0) {
+        roomChannel.sendMessage($input.val());
+        $input.val(“”);
+    }
+  }
+});
 });
